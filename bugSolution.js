8586052630730a8908e096b4b65476307ec2beba
BugHunter-X <askzenai@gmail.com>
2025-01-25 05:26:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,32 @@
+```javascript
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact/:id?" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
+
+function Contact() {
+  const { id } = useParams();
+  return (
+    <div>Contact {id ? `(ID: ${id})` : ''}</div>
+  );
+}
+
+export default App;
+```
